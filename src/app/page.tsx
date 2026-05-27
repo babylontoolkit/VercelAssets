b08815d826f0a24c8d6dfcb2c1dc447f57ac0e92
MackeyK24 <MackeyK24@gmail.com>
2026-05-26 15:18:32 -1000

    Getting Started Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,8 +47,8 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://github.com/BabylonJS/BabylonToolkit"
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[188px]"
+            href="https://www.babylontoolkit.com/documentation"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -59,7 +59,7 @@ export default function Home() {
               width={16}
               height={16}
             />
-            Babylon Toolkit
+            Getting Started
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
