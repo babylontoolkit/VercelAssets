030f60b17f98bf0b5c5143e3a3a5e939107b2d48
MackeyK24 <MackeyK24@gmail.com>
2026-06-27 12:35:28 -1000

    IBL Compression Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const handlePlayDemo = () => {
     navigate('/play', {
       gameMode: 'PlayerControllerDemo',
-      sceneUrl: 'https://repo.babylontoolkit.com/playground/samplescene.gltf',
+      sceneUrl: 'https://repo.babylontoolkit.com/playground/samplescene.gz.gltf',
       reloadPage: (process.env.NODE_ENV !== "development") // Note: In development mode, we want to preserve the state of hot-reloading, so we won't reload the page. In production, we can reload to ensure a clean state.
     });
   };
