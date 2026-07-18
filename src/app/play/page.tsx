edefbdf8091a403c8d3dd400b639c6a6d2008d75
MackeyK24 <MackeyK24@gmail.com>
2026-07-17 15:14:32 -1000

    Project Template Update

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -5,7 +5,7 @@ import BabylonSceneViewer from "@/babylon/system/babylon";
 export default function Play() {
   return (
     <Suspense fallback={<DefaultBabylonPreloader />}>
-      <BabylonSceneViewer fullPage={true} allowQueryParams={true} enableCustomOverlay={false} />
+      <BabylonSceneViewer fullPage={true} allowQueryParams={true} enableCustomOverlay={true} />
     </Suspense>
   );
 }
